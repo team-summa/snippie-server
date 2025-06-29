94b43913440c7046bb42d084d6acc835eaa65179
serim Kwon <serimkwon1@naver.com>
2025-06-29 14:38:08 +0900

    chore: 프로젝트 초기 세팅 (Spring Boot + 의존성 추가)

---
+++ src/main/java/com/snippie/backend/SnippieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snippie.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippieBackendApplication.class, args);
+	}
+
+}
