80ab5bd45134153cbaed268dc44f5f4859a3dbbd
AshtonSW <sprauncy76@gmail.com>
2025-07-02 23:38:01 +0900

    refactor: Config 폴더 구조 변경에 따른 Config 클래스 이동
    - SecurityConfig.java, SwaggerConfig.java 클래스를 common/config로 이동

--- src/main/java/com/snippie/backend/common/config/SecurityConfig.java
+++ src/main/java/com/snippie/backend/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.snippie.backend.config;
+package com.snippie.backend.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api/summaries" // 임시, 추후 삭제
                         )
                         .permitAll()
                         .anyRequest().authenticated()
