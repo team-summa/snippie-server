efdc3c51dffad45810137a049d26c0d0ee3c4d21
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-07-06 02:27:10 +0900

    [Feature] 유저 정보 조회 API 구현 (#34)
    import

--- src/main/java/com/snippie/backend/user/controller/UserController.java
+++ src/main/java/com/snippie/backend/user/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.snippie.backend.user.controller;
 
-import com.snippie.backend.user.domain.User;
+import com.snippie.backend.auth.security.UserPrincipal;
 import com.snippie.backend.user.dto.UserResponseDto;
 import com.snippie.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
