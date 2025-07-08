af64ab4bbe0934ef8813d1ca6de956e9e8d3e562
serim Kwon <serimkwon1@naver.com>
2025-07-09 02:13:48 +0900

    feat: 요약 삭제 API 구현 (#52)

--- src/main/java/com/snippie/backend/user/service/UserServiceImpl.java
+++ src/main/java/com/snippie/backend/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.snippie.backend.common.exception.ErrorCode;
 import com.snippie.backend.common.exception.SnippieException;
 import com.snippie.backend.summary.domain.Summary;
 import com.snippie.backend.summary.domain.SummaryType;
+import com.snippie.backend.summary.dto.SummaryRequestDto;
 import com.snippie.backend.summary.repository.SummaryRepository;
 import com.snippie.backend.user.domain.User;
 import com.snippie.backend.user.dto.SummaryDto;
@@ -60,6 +61,15 @@ public class UserServiceImpl implements UserService {
         }
 
         return SummaryDto.of(summary);
+    }
+
+    @Override
+    public void deleteSummary(Long id) {
+        if (!summaryRepository.existsById(id)) {
+            throw new SnippieException(ErrorCode.SUMMARY_NOT_FOUND);
+        }
+
+        summaryRepository.deleteById(id);
     }
 
 
