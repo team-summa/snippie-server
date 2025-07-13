5aac257ce0aa97ac30424595180078d295d3959f
AshtonSW <sprauncy76@gmail.com>
2025-07-13 17:16:20 +0900

    fix: RequestMapping 경로 수정
    - /auth -> /api/auth

--- src/main/java/com/snippie/backend/auth/controller/AuthController.java
+++ src/main/java/com/snippie/backend/auth/controller/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Operation(
